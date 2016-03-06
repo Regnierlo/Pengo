754930000ec926f7f4417ce7ff39f113feeb20f1
loisr <loisr@192.168.1.77>
2016-03-06 15:33:03 +0100

    Reset all

--- src/pengo/Pengo.java
+++ src/pengo/Pengo.java
@@ -5,10 +5,6 @@
  */
 package pengo;
 
-import Personnages.P_Pengo;
-import Ressources.Positions;
-import javax.swing.JFrame;
-
 /**
  *
  * @author loisr
@@ -19,7 +15,7 @@ public class Pengo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Game_Engine();
+        
         
         
         //System.out.print("\033[2J\033[1;1H"); // Clear console
