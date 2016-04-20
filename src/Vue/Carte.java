ceea4a9b2b4916aa9f33d533838ab87d019d00ee
Marie <Marie@192.168.1.96>
2016-04-20 10:51:48 +0200

    Merge origin/master
    Conflicts:
    	src/GestionJeux/GameEngine.java
    	src/Ressources/BlocGlace.java

--- src/Vue/Carte.java
+++ src/Vue/Carte.java
@@ -7,69 +7,99 @@ import Ressources.*;
 import java.awt.Graphics ;
 import java.awt.Graphics2D ;
 
-import java.awt.Font ;
-import java.awt.Color ;
+import java.awt.Image;
+import java.io.File;
+
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 
 import javax.swing.JPanel;
 
 public class Carte extends JPanel{
     private GameEngine jesus ;
-    private MyImage[][] tabImage ;
+    private Image[][] tabImage ;
+    private Image image ;
     
     public Carte(GameEngine gameEngine){
         jesus = gameEngine ;
-        tabImage = new MyImage[16][10] ;
+        tabImage = new Image[16][10] ;
+
     }
     
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g ;
-        g2.drawImage(tabImage[0][0].getImg(),0, 0, tabImage[0][0].getObserver());
-        for(int i = 0 ; i < 16 ; i++){
+       for(int i = 0 ; i < 16 ; i++){
             for(int j = 0 ; j < 10 ; j++){
                 if(tabImage[i][j]!=null){
-                    g2.drawImage(tabImage[i][j].getImg(),i*32, j*32, tabImage[i][j].getObserver());
+                    System.out.println("qfkjlm");
+                    g2.drawImage(tabImage[i][j],i*32, j*32, this);
                 }
             }
         }
     }
     
+    
     public void setImages(String[][] s){
-        for(int i = 0 ; i < s.length ; i++){
-            for(int j = 0 ; j < s[0].length ; j++){
+        for(int i = 0 ; i < 16 ; i++){
+            for(int j = 0 ; j < 10 ; j++){
                 switch (s[i][j]){
                     case "P" : {
                         if(jesus.getPengo() instanceof P_Pengo){
-                            
-                            tabImage[i][j] = jesus.getPengo().getMyImage() ;
+                            try{
+                                image= ImageIO.read(new File("src/Images/Pengo_Derriere_Pied_Droit.png")) ;
+                                tabImage[i][j] = image ;
+                            }catch(IOException e){
+                                e.printStackTrace() ;
+                            }
                         }
                     } break ;
                     case "E" : {
                         if(jesus.getSnoBees(new Coordonnees(i,j)) instanceof SnoBees){
-                            tabImage[i][j] = jesus.getSnoBees(new Coordonnees(i,j)).getMyImage() ;
+                           try{
+                                image= ImageIO.read(new File("src/Images/SnoBees_Droit.png")) ;
+                                tabImage[i][j] = image ;
+                            }catch(IOException e){
+                                e.printStackTrace() ;
+                            }
                         }
                     } break ;
                     case "M" : {
                         if(jesus.getMur(new Coordonnees(i,j)) instanceof Mur){
-                            tabImage[i][j] = jesus.getMur(new Coordonnees(i,j)).getMyImage() ;
+                           try{
+                                image= ImageIO.read(new File("src/Images/mur_de_gauche.jpg")) ;
+                                tabImage[i][j] = image ;
+                            }catch(IOException e){
+                                e.printStackTrace() ;
+                            }
                         }
                         
                     } break ;
                     case "G" : {
                         if(jesus.getBloc(new Coordonnees(i,j)) instanceof BlocGlace){
-                            tabImage[i][j] = jesus.getBloc(new Coordonnees(i,j)).getMyImage();
+                            try{
+                                image= ImageIO.read(new File("src/Images/bloc_normal.png")) ;
+                                tabImage[i][j] = image ;
+                            }catch(IOException e){
+                                e.printStackTrace() ;
+                            }
                         }
                     } break ;
                     case "S":
                         if(jesus.getBloc(new Coordonnees(i, j)) instanceof BlocSpecial){
-                            tabImage[i][j] = jesus.getBloc(new Coordonnees(i, j)).getMyImage();
+                            try{
+                                image= ImageIO.read(new File("src/Images/companion_cube.png")) ;
+                                tabImage[i][j] = image ;
+                            }catch(IOException e){
+                                e.printStackTrace() ;
+                            }
                         }
                         break;
                 }
             }
         }
-       
+    repaint();   
     }
 }
---
+++ src/Vue/Picture.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vue;
+
+import java.awt.Image ;
+import java.awt.Image ;
+import java.io.File ;
+
+import java.io.IOException ;
+import javax.imageio.ImageIO ;
+
+public class Picture {
+    Image[][] tabImages ;
+
+    public Picture(){
+
+        tabImages = new Image[2][10] ;
+        
+
+    }
+}
